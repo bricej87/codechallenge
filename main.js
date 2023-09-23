46a8982461b0c2592f16f4341ae94eecfae7cf35
bricej87 <132966908+bricej87@users.noreply.github.com>
2023-09-22 21:10:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -41,3 +41,32 @@ const largestPair = function (array) {
 
 
 //Problem 6
+const scoreScrabble = function (str) {
+    let pointTotal = 0;
+    str = str.toLowerCase();
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u' || str[i] === 'l' || str[i] === 'n' || str[i] === 'r' || str[i] === 's' || str[i] === 't') {
+            pointTotal += 1;
+        }
+        else if (str[i] === 'd' || str[i] === 'g') {
+            pointTotal += 2;
+        }
+        else if (str[i] === 'b' || str[i] === 'c' || str[i] === 'm' || str[i] === 'p') {
+            pointTotal += 3;
+        }
+        else if (str[i] === 'f' || str[i] === 'h' || str[i] === 'v' || str[i] === 'w' || str[i] === 'y') {
+            pointTotal += 4;
+        }
+        else if (str[i] === 'k') {
+            pointTotal += 5;
+        }
+        else if (str[i] === 'j' || str[i] === 'k') {
+            pointTotal += 8;
+        }
+        else if (str[i] === 'q' || 'z') {
+            pointTotal += 10;
+        }
+
+    }
+    return pointTotal
+};
